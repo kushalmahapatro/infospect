f09bbd905687d096aac3aa6cc2f0775f5bfe6a03
kushalm <kushalmahapatro@gmail.com>
2023-08-20 15:53:55 +0400

    chore: Screenshot number reduced

--- lib/utils/common_widgets/app_adaptive_dialog.dart
+++ lib/utils/common_widgets/app_adaptive_dialog.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppAdaptiveDialog {
@@ -8,27 +11,77 @@ class AppAdaptiveDialog {
     required String body,
     required VoidCallback onPositiveActionClick,
   }) {
-    showAdaptiveDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog.adaptive(
-          title: Text(title),
-          content: Text(body),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, '${tag}_cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                onPositiveActionClick.call();
-                Navigator.pop(context, '${tag}_clear');
-              },
-              child: const Text('Go ahead'),
-            ),
-          ],
-        );
-      },
-    );
+    final String version = Platform.version;
+    if (version.split(' ').first.removeDotAndToInt >= 310) {
+      showAdaptiveDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog.adaptive(
+            title: Text(title),
+            content: Text(body),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, '${tag}_cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  onPositiveActionClick.call();
+                  Navigator.pop(context, '${tag}_clear');
+                },
+                child: const Text('Go ahead'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          if (Platform.isMacOS || Platform.isIOS) {
+            return CupertinoAlertDialog(
+              title: Text(title),
+              content: Text(body),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, '${tag}_cancel'),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    onPositiveActionClick.call();
+                    Navigator.pop(context, '${tag}_clear');
+                  },
+                  child: const Text('Go ahead'),
+                ),
+              ],
+            );
+          } else {
+            return AlertDialog(
+              title: Text(title),
+              content: Text(body),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, '${tag}_cancel'),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    onPositiveActionClick.call();
+                    Navigator.pop(context, '${tag}_clear');
+                  },
+                  child: const Text('Go ahead'),
+                ),
+              ],
+            );
+          }
+        },
+      );
+    }
   }
+}
+
+extension StringExtension on String {
+  int get removeDotAndToInt => int.tryParse(replaceAll('.', '')) ?? 0;
 }
